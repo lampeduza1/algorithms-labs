46f6c9d7028d9d13aa8343d4574afe8f281c9e30
DarkKillerGanjubas <artyomviktorov0@gmail.com>
2021-09-08 19:18:40 +0300

    task1 pending 2

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <cstdio>
 #include <algorithm>
+#include <set>
+#include <iterator>
 
 using namespace std;
 
@@ -9,20 +11,26 @@ typedef long long ll;
 vector<ll> Nvi;
 vector<ll> Mvi;
 
-//struct greater
-//{
-//  template<class T>
-//  bool operator()(T const &a, T const &b) const { return a > b; }
-//};
-
-ll binary_search_index(vector<ll> v, ll data)
+ll binary_search_index(vector<ll> v, ll data, bool flag)
 {
-    auto it = lower_bound(v.begin(), v.end(), data);
-    if (it == v.end() || *it != data) {
-        return -1;
-    } else {
-        return distance(v.begin(), it);
+    // auto it = lower_bound(v.begin(), v.end(), data);
+    // if (flag)
+    //     it = upper_bound(v.begin(), v.end(), data);
+    // if (it == v.end()) {
+    //     return -1;
+    // } else {
+    //     printf("index: %lld\n", distance(v.begin(), it));
+    //     return distance(v.begin(), it); // изменить на O(1)
+    // }
+    ll counter = 0;
+    for (const auto& i : v) {
+        if (flag) {
+            if (i >= data) counter++;
+        } else {
+            if (i <= data) counter++;            
+        }
     }
+    return counter;
 }
 
 int main()
@@ -40,21 +48,29 @@ int main()
     }
     sort(Nvi.begin(), Nvi.end());
     sort(Mvi.begin(), Mvi.end());
-    ll min_cost = Mvi[0];
-    ll max_cost = *(Nvi.end() - 1);
+    ll min_cost = min(Nvi[0], Mvi[0]);
+    ll max_cost = max(*(Nvi.end() - 1), *(Mvi.end() - 1));
     ll trade = -1;
     ll cost = -1;
-    for (ll i_cost = min_cost; i_cost <= max_cost; i_cost++) {
-        ll index_a = binary_search_index(Nvi, i_cost);
-        ll a = 0;
-        if (index_a != -1) a = index_a + 1;
-        ll index_b = binary_search_index(Mvi, i_cost);
-        ll b = 0;
-        if (index_b != -1) b = Mvi.size() - (index_b + 1);
+    ll a = 0, b = 0;
+    printf("min_cost: %lld\n", min_cost);
+    printf("max_cost: %lld\n\n", max_cost);
+    vector<ll> NMunion_v; 
+    set_union(Nvi.begin(), Nvi.end(),
+        Mvi.begin(), Mvi.end(),
+        back_inserter(NMunion_v));
+    set<ll> NMunion(NMunion_v.begin(), NMunion_v.end());
+    for (const auto &i_cost : NMunion) {
+        a = binary_search_index(Nvi, i_cost, false);
+        b = binary_search_index(Mvi, i_cost, true);
         if (trade < (min(a, b) * i_cost)) {
             trade = min(a, b) * i_cost;
             cost = i_cost;
         }
+        printf("i_cost: %lld\n", i_cost);
+        printf("a i_cost: %lld\n", a);
+        printf("b i_cost: %lld\n", b);
+        printf("TRADE: %lld\n\n", trade);
     }
     printf("%lld %lld\n", cost, trade);
     return 0;
